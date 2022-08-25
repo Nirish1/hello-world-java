a102dc0f5cd3759a7f5996a7356c06a9b29f88dc
Nirish1 <111074025+Nirish1@users.noreply.github.com>
2022-08-25 16:24:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("welcome to Nirish");
+    System.out.println("welcome to siree");
   }
 }
