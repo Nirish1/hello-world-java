704095f832f7723bd9f1dbd820eeeb5439019bcc
Nirish1 <111074025+Nirish1@users.noreply.github.com>
2022-08-26 11:22:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
+    for (i=0; i<10; i++)
+    System.out.println(i);
+    
     System.out.println("Hello world!");
     System.out.println("welcome to Nirish");
     System.out.println("welcome to siree");
